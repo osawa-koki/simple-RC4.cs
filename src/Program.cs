5bd2b9ee5f6e664e28ce5af5088424a560ace33d
osawa-koki <osawa-koki@example.com>
2023-03-07 10:18:04 +0900

    プログラムの大枠が完成した。

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Key: ");
+string key = Console.ReadLine()!;
+Console.Write("Message: ");
+string message = Console.ReadLine()!;
+
+Console.WriteLine($"key: {key}");
+Console.WriteLine($"message: {message}");
+
+Console.Clear();
+
+string encryptResult = RC4.Encrypt(message, key);
+string decryptResult = RC4.Decrypt(encryptResult, key);
+
+Console.WriteLine($"original: {message}");
+Console.WriteLine($"encrypt: {encryptResult}");
+Console.WriteLine($"decrypt: {decryptResult}");
